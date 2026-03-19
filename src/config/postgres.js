69e63f7d9df20da7b8abe048ccbb6ddf95407aee
tjayearl <iamtjayearl@gmail.com>
2026-03-19 13:21:03 +0300

    modified:   src/config/postgres.js

--- src/config/postgres.js
+++ src/config/postgres.js
@@ -1,20 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.PG_HOST || 'localhost',
-  port: process.env.PG_PORT || 5432,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
-});
-
-// Test connection
-pool.on('connect', () => {
-  console.log('✓ PostgreSQL connected');
-});
-
-pool.on('error', (err) => {
-  console.error('PostgreSQL connection error:', err);
+  ssl: {
+    rejectUnauthorized: false // Required for Render PostgreSQL
+  }
 });
 
 module.exports = pool;
